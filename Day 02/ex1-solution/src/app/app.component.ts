7c50fd1347af82305498a8fbd0e4efa7bc1245da
Kobi Hari <kobi2294@yahoo.com>
2021-01-20 14:25:01 +0200

    ex1 solution completed

--- Day 02/ex1-solution/src/app/app.component.ts
+++ Day 02/ex1-solution/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly question = 'How much is 6 * 7';
+  readonly answers = ['36', '42', '49', '56'];
+  readonly correctAnswer = '42';
+
+  selectedAnswer: string | null = null;
+  isCorrect: boolean | null = null;
+  wasAnswered: boolean = false;
+
+  selectAnswer(value: string) {
+    if (this.wasAnswered) return;
+
+    this.selectedAnswer = value;
+    this.isCorrect = (value === this.correctAnswer);
+    this.wasAnswered = true;
+  }
+
 }
